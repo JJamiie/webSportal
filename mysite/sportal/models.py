1952dff6092181a658321022e71ea5f303055eb7
JJamie Rashata <jjamie_08@hotmail.com>
2016-03-09 12:23:23 +0700

    - add role user profile

--- mysite/sportal/models.py
+++ mysite/sportal/models.py
@@ -1,5 +1,21 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+	
+	GENDER = (
+		('0','Male'),
+		('1','Female')
+	)
+
+	user = models.ForeignKey(User,on_delete=models.CASCADE)
+	firstname = models.CharField(max_length=200)
+	lastname = models.CharField(max_length=200)
+	gender = models.PositiveSmallIntegerField(choices=GENDER)
+	about_me = models.CharField(max_length=1000)
+	number_like = models.PositiveSmallIntegerField()
+	place = models.CharField(max_length=200)
+
